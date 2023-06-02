dbb446930c9f3a368aa36ac47625ddb8e7748f76
최지연 <choi02647@naver.com>
2023-06-02 13:30:08 +0900

    Initial Commit

---
+++ CafeApp/CafeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeAppApp.swift
+//  CafeApp
+//
+//  Created by 최지연/클라이언트 on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct CafeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
